d575f369a1a17add19af2d523017c8d33b7479e3
kearakha <keanuraka14@gmail.com>
2025-11-07 19:37:58 +0700

    fix for login in fe

---
+++ config/cors.php
@@ -0,0 +1,36 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Cross-Origin Resource Sharing (CORS) Configuration
+    |--------------------------------------------------------------------------
+    |
+    | Here you may configure your settings for cross-origin resource sharing
+    | or "CORS". This determines what cross-origin operations may execute
+    | in web browsers. You are free to adjust these settings as needed.
+    |
+    | To learn more: https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS
+    |
+    */
+
+    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+
+    'allowed_methods' => ['*'],
+
+    // INI BAGIAN YANG DIPERBAIKI (sesuai error Anda)
+    'allowed_origins' => ['http://localhost:3000'],
+
+    'allowed_origins_patterns' => [],
+
+    'allowed_headers' => ['*'],
+
+    'exposed_headers' => [],
+
+    'max_age' => 0,
+
+    // INI BAGIAN KEDUA YANG DIPERBAIKI (sesuai error Anda)
+    'supports_credentials' => true,
+
+];
