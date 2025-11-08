8c0b7749eb36fd0b010a6320c25e1406b6f61332
mutiarasrn <mutiarasetya15@gmail.com>
2025-11-08 23:00:24 +0700

    feat: expose document fields di ArchiveResource

--- app/Http/Resources/ArchiveResource.php
+++ app/Http/Resources/ArchiveResource.php
@@ -19,10 +19,13 @@ class ArchiveResource extends JsonResource
             'updated_at'     => $this->updated_at?->toAtomString(),
             'document'       => $this->whenLoaded('document', function () {
                 return [
-                    'id'       => $this->document->id,
-                    'tentang'  => $this->document->tentang,
-                    'status'   => $this->document->status,
-                    'file_url' => $this->document->file_upload ? url('storage/'.$this->document->file_upload) : null,
+                    'id'                => $this->document->id,
+                    'jenis_dokumen'     => $this->document->jenis_dokumen,
+                    'nomor_ditetapkan'  => $this->document->nomor_ditetapkan,
+                    'tanggal_ditetapkan'=> $this->document->tanggal_ditetapkan?->format('Y-m-d'),
+                    'tentang'           => $this->document->tentang,
+                    'status'            => $this->document->status,
+                    'file_url'          => $this->document->file_upload ? url('storage/'.$this->document->file_upload) : null,
                 ];
             }),
         ];
